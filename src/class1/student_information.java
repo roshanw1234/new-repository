fac3c2b5186f40c0c472692093f4b0dd9e8aacf4
roshanwankhade326 <roshanwankhade326@LAPTOP-P42SCIO1>
2023-06-06 00:13:16 +0530

    my first cummit

---
+++ src/class1/student_information.java
@@ -0,0 +1,29 @@
+package class1;
+
+public class student_information {
+	
+	public static void main(String[] org)
+	{
+		String name="roshan ";
+		byte rollno=54;
+		float percentage=98.98f;
+		int total=100;
+		char grade='A';
+		String address="pune";
+		String school_name="english high school";
+		boolean result= true;
+		
+		
+		
+		System.out.println(name);
+		System.out.println(rollno);
+		System.out.println(percentage + "%");
+		System.out.println(total);
+		System.out.println(grade);
+		System.out.println(address);
+		System.out.println(school_name);
+		System.out.println(result);
+		
+	
+	}
+}
